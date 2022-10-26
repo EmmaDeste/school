3bcb44c6800db80500f0b90c0dc6b52704a51f57
soanja <78196995+soanja@users.noreply.github.com>
2022-10-26 16:22:20 +0200

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,13 @@
+public class Department {
+
+    private double grade;
+
+    public void printStudent (String studentNumber){
+        System.out.println("stud number : " + studentNumber);
+    }
+
+
+    public void printTeacher (String staffNumber){
+        System.out.println("stud number : " + staffNumber);
+    }
+}
