d198bb5b5b456fd0033d46db0fa6e5944e5b6690
Shadow032020 <72571768+Shadow032020@users.noreply.github.com>
2022-10-26 15:59:59 +0200

    Add files via upload

---
+++ src/efrei/fr/domain/Subject.java
@@ -0,0 +1,13 @@
+public class Subject{
+    private String subjectCode ;
+    private String subjectDescription ;
+    private double grade;
+
+    public double averageGrade(String subjectCode){
+        return double(subjectCode);
+    }
+
+    public double averageGradePerSubjectAndDepartment(){
+        return double(subjectCode);
+    }
+}
